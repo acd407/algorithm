c9bb9f6785f68ae142e6bd73575d36fb6f65a329
mathtimes <mathtimes@foxmail.com>
2022-10-27 16:30:29 +0800

    add a test dir
    Signed-off-by: mathtimes <mathtimes@foxmail.com>

--- matrix/test/ma_lu.c
+++ matrix/test/ma_lu.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include "ma_mat.h"
+#include "../ma_mat.h"
 
 int main() {
     double mat[16] = {
@@ -10,14 +9,29 @@ int main() {
         2,  6,  9,  18, 
         6,  15, 18, 40
     };
+
     ma_mat mtx = {4, 4, mat};
+    puts("Original Matrix: ");
     ma_pnt(&mtx);
     putchar('\n');
+
     ma_mat L = {0, 0, 0};
     ma_mat U = {0, 0, 0};
     ma_lu(&mtx, &L, &U);
+
+    puts("L: ");
     ma_pnt(&L);
     putchar('\n');
+
+    puts("U: ");
     ma_pnt(&U);
+    putchar('\n');
+
+    printf("%g = %g * %g\n", 
+        ma_det(&mtx), 
+        ma_det(&L), 
+        ma_det(&U)
+    );
+
     return 0;
 }
