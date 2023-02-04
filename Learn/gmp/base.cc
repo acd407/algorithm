70fd563ddf9298e589f40f9d3a6486cafe93208b
mathtimes <mathtimes@foxmail.com>
2023-02-04 22:07:15 +0800

    gmp
    Signed-off-by: mathtimes <mathtimes@foxmail.com>

--- Learn/gmp/base.cc
+++ Learn/gmp/base.cc
@@ -1,9 +1,14 @@
+#include <fmt/core.h>
 #include <gmpxx.h>
 #include <iostream>
 
-int main() {
-    mpf_class a{"3326.58"};
-    std::cout << a << std::endl;
-    a.
+int main()
+{
+    mpz_class res(1);
+    int i(1);
+    for (i=1;i!=100;++i)
+        res*=i;
+    //fmt::print("{}", res.get_str(16));
+    std::cout << res << std::endl;
     return 0;
 }
